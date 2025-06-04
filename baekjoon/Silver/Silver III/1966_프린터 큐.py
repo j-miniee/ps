226b87762c09a2c0d3c8b4e928809e3d1208fa83
j-miniee <163227546+j-miniee@users.noreply.github.com>
2025-06-04 14:29:01 +0000

    from collections import deque

--- baekjoon/Silver/Silver III/1966_프린터 큐.py
+++ baekjoon/Silver/Silver III/1966_프린터 큐.py
@@ -2,11 +2,11 @@ from collections import deque
 
 test = int(input())
 
-cnt = 0
 for _ in range(test):
     n, m = map(int, input().split())
     queue = deque(map(int, input().split()))
 
+    cnt = 0
     while queue:
         if queue[0] != max(queue):
             n = queue.popleft()
@@ -14,9 +14,13 @@ for _ in range(test):
         else:
             queue.popleft()
             cnt += 1
+
             if m == 0:
                 break
-        
 
+        if m > 0:
+            m -= 1
+        else:
+            m = len(queue) -1
 
-
+    print(cnt)
