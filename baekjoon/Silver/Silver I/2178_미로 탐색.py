6bb7a642b55f689b6cd1d125781ca767ee5b10b4
j-miniee <163227546+j-miniee@users.noreply.github.com>
2025-06-08 13:17:58 +0000

    최단 거리/ nx, ny = x + dx, y + dy

---
+++ baekjoon/Silver/Silver I/2178_미로 탐색.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)] 
+
+def bfs(x, y):
+    directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for dx, dy in directions:
+            nx , ny = x + dx, y + dy
+
+            if 0<= nx <n and 0<= ny <m and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
