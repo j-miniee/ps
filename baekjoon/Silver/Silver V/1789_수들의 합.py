c9c69a1e13eee6fb524c7176828d114b4f8f87e4
j-miniee <163227546+j-miniee@users.noreply.github.com>
2025-06-13 05:12:51 +0000

    **탐욕법

--- baekjoon/Silver/Silver V/1789_수들의 합.py
+++ baekjoon/Silver/Silver V/1789_수들의 합.py
@@ -3,9 +3,12 @@ s = int(input())
 total = 0
 cnt = 0
 
-for i in range(1, s):
+for i in range(1, s+1):
     total += i 
     cnt += 1
     if total > s:
         print(cnt-1)
         break
+
+else:
+    print(cnt)
